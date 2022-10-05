c3d20e7e0b0e528e849de3cef1d4d30ac86acb36
davidpb <david.perez.barroso@estudiantat.upc.edu>
2022-10-05 17:46:30 +0200

    Prueba2

--- electricity_front/lib/main.dart
+++ electricity_front/lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo'),
+      home: const MyHomePage(title: 'Flutter Demo APP'),
     );
   }
 }
