9c3bf4c9c4c999273cb00aa46696cad4002553fd
david <david.peba123@gmail.com>
2022-10-12 22:19:01 +0200

    Config commit

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/david/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/david/Programacion/ElectriCity_Front/electricity_front"
+export "FLUTTER_APPLICATION_PATH=/Users/david/Programacion/ElectriCity_Front"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/david/Programacion/ElectriCity_Front/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/david/Programacion/ElectriCity_Front/.dart_tool/package_config.json"
