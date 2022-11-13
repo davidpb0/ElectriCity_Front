bc7a682aa4cff84c8a4b6d7873121bec8f6c176d
david <david.peba123@gmail.com>
2022-11-13 14:36:53 +0100

    Info windows bug fix

--- lib/ui/components/info_bicing_station_window.dart
+++ lib/ui/components/info_bicing_station_window.dart
@@ -12,73 +12,75 @@ class InfoBicingStationWindow extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height: 500,
-        decoration: BoxDecoration(
-          boxShadow: [BoxShadow(
-            offset: Offset(0, 2),
-            blurRadius: 1,
+    return SingleChildScrollView(
+      child: Container(
+          height: 500,
+          decoration: BoxDecoration(
+            boxShadow: [BoxShadow(
+              offset: Offset(0, 2),
+              blurRadius: 1,
 
-          )],
-            border: Border.all(color: Colors.white),
-            borderRadius: BorderRadius.circular(12),
-          color: Colors.white
-        ),
-          child: Column(children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(12), topRight: Radius.circular(12)),
-                color: Colors.blue
-              ),
-              width: MediaQuery.of(context).size.width,
-              child: Text(
-                addres,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
+            )],
+              border: Border.all(color: Colors.white),
+              borderRadius: BorderRadius.circular(12),
+            color: Colors.white
+          ),
+            child: Column(children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(12), topRight: Radius.circular(12)),
+                  color: Colors.blue
+                ),
+                width: MediaQuery.of(context).size.width,
+                child: Text(
+                  addres,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
                 ),
               ),
-            ),
-            Container(
-                color: Colors.white,
-                child: Column(
-                  //padding
-                  children: [
-                    Row(
-                      children: [
-                        Padding(
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 10, vertical: 6),
-                            child: Icon(TestIcons.ebike, size: 20)),
-                        const Text("Bicicletes elèctriques disponibles: "),
-                        Text(belec.toString())
-                      ],
-                    ),
-                    Row(
-                      children:[
-                        Padding(
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 10, vertical: 6),
-                            child: Icon(TestIcons.bike, size: 20)),
-                        Text("Bicicletes mecàniques disponibles: "),
-                        Text(bmech.toString())
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 10, vertical: 6),
-                            child: Icon(TestIcons.bicing_parking, size: 20)),
-                        Text("Espais disponibles: "),
-                        Text(slots.toString())
-                      ],
-                    )
-                  ],
-                )),
-          ]),
-        );
+              Container(
+                  color: Colors.white,
+                  child: Column(
+                    //padding
+                    children: [
+                      Row(
+                        children: [
+                          Padding(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 10, vertical: 6),
+                              child: Icon(TestIcons.ebike, size: 20)),
+                          const Text("Bicicletes elèctriques disponibles: "),
+                          Text(belec.toString())
+                        ],
+                      ),
+                      Row(
+                        children:[
+                          Padding(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 10, vertical: 6),
+                              child: Icon(TestIcons.bike, size: 20)),
+                          Text("Bicicletes mecàniques disponibles: "),
+                          Text(bmech.toString())
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 10, vertical: 6),
+                              child: Icon(TestIcons.bicing_parking, size: 20)),
+                          Text("Espais disponibles: "),
+                          Text(slots.toString())
+                        ],
+                      )
+                    ],
+                  )),
+            ]),
+          ),
+    );
   }
 
 }
