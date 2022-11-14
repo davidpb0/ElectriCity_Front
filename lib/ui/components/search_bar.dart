17c21f1a87616d34e7bd2891e2ad39e7282116de
granolagranola <57772949+granolagranola@users.noreply.github.com>
2022-11-14 20:02:33 +0100

    Hotfix listview sincronización con API (Bicing) + Posible vista Recharge

---
+++ lib/ui/components/search_bar.dart
@@ -0,0 +1,32 @@
+import 'package:electricity_front/ui/views/listpage.dart';
+import 'package:electricity_front/ui/views/profilepage.dart';
+import 'package:flutter/material.dart';
+
+import '../views/mapa.dart';
+import 'info_charge_station_window.dart';
+
+
+class SearchBar extends StatefulWidget{
+  const SearchBar({super.key});
+
+
+  @override
+  _SearchBarState createState() => _SearchBarState();
+
+}
+
+class _SearchBarState extends State<SearchBar>{
+  late String search;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+
+  }
+
+
+}
