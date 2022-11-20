fbcdba63ea2000eff69412e1fbed0b163e27ac40
granolagranola <57772949+granolagranola@users.noreply.github.com>
2022-11-20 23:38:24 +0100

    Signup con Token y formulario validado con mensajes de errores

--- lib/core/controllers/signupController.dart
+++ lib/core/controllers/signupController.dart
@@ -17,7 +17,10 @@ class SignupController {
   var _email = "";
   String? _emailError;
 
-  var _passwd = "";
+  var _passwd1 = "";
+
+  var _passwd2 = "";
+
   String? _passwdError;
 
   factory SignupController(){
@@ -36,15 +39,24 @@ class SignupController {
     _email = _mail;
   }
 
-  void setPasswd(String _pwd1, String _pwd2) {
-    if (_pwd1 == _pwd2) {
-      print("Password matching");
-      _passwd = _pwd1;
-      return;
-    }
-    print("Password not matching");
-    _passwdError = "Password not matching";
+  void setPasswd(String _pwd1) {
+      _passwd1 = _pwd1;
+  }
 
+  void setPasswdRepeat(String _pwd1) {
+    _passwd2 = _pwd1;
+  }
+
+  bool validatePassword(){
+    if (_passwd1 == _passwd2) {
+      print("Password matching");
+      return true;
+    }
+    else {
+      print("Password not matching");
+      _passwdError = "Password not matching";
+      return false;
+    }
   }
 
 
@@ -52,15 +64,18 @@ class SignupController {
     var data={
       "username":this._username,
       "email":this._email,
-      "password":this._passwd,
+      "password":this._passwd1,
     };
 
     Response res = await _apiService.postData(data, 'signup');
     var body = json.decode(res.body);
-    if (body['message'] == 'Successfull login'){
+    if (res.statusCode == 200){
       _loggedUser = User.fromJson(body);
       Navigator.of(ctext).pushReplacementNamed('/home');
-    };
+    }
+    else{
+      _emailError = 'This email is already being used';
+    }
     print(res.statusCode);
     print(res.body.toString());
     //print(body['user']['tokens'].first);
@@ -71,7 +86,8 @@ class SignupController {
   void printData() {
     print("Username: " + _username);
     print("Email: " + _email);
-    print("Password: " + _passwd);
+    print("Password: " + _passwd1);
+    print("Password repeat: " + _passwd2);
   }
 
   String? usernameError() {
