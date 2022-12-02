8cc9dcfc898e5f5d9e5a1115ca3ffd345a016fd3
granolagranola <19CZeppeli98>
2022-12-02 21:16:18 +0100

    l10n implemented

---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_en.dart
@@ -0,0 +1,24 @@
+import 'app_localizations.dart';
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get login_welcomeback => 'Welcome back!';
+
+  @override
+  String get login_forgotpassword => 'Forgot password?';
+
+  @override
+  String get login_signin => 'Sign In';
+
+  @override
+  String get login_clickhere => 'Click here';
+
+  @override
+  String get login_notamember => 'Not a member?';
+
+  @override
+  String get login_signup => 'Sign up';
+}
---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_es.dart
@@ -0,0 +1,24 @@
+import 'app_localizations.dart';
+
+/// The translations for Spanish Castilian (`es`).
+class AppLocalizationsEs extends AppLocalizations {
+  AppLocalizationsEs([String locale = 'es']) : super(locale);
+
+  @override
+  String get login_welcomeback => 'Bienvenido!';
+
+  @override
+  String get login_forgotpassword => 'Olvidaste tu contraseña?';
+
+  @override
+  String get login_signin => 'Inicia sesión';
+
+  @override
+  String get login_clickhere => 'Haz click aquí';
+
+  @override
+  String get login_notamember => 'No eres miembro aún?';
+
+  @override
+  String get login_signup => 'Únete';
+}
