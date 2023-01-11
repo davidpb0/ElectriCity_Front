958722bf850eaeead52d83db97e9d1556e972168
david <david.peba123@gmail.com>
2023-01-11 22:17:22 +0100

    cleaner code

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+import 'core/controllers/mapa_controller.dart';
+
 
 
 void main() async {
   runApp(const MyApp());
   HttpOverrides.global = MyHttpOverrides();
-  //await MapaController().initBD();
+  MapaController().initBD();
 }
 
 class MyApp extends StatelessWidget {
